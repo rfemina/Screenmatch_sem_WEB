9e195c9421ca531606dd72c8c32d64509125ad51
Rafael Femina <rfemina2@gmail.com>
2024-02-16 15:59:14 -0300

    Inicio do projeto Spring

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,32 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumindo = new ConsumoApi();
+		var json = consumindo.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=5e90d3e0");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+//		json = consumindo.obterDados("https://coffee.alexflipnote.dev/random.json");
+//		System.out.println(json);
+
+
+
+	}
+}
